9d5c2f6d6320ff633d18f845f5ea7210159783d3
augustobx <130471886+augustobx@users.noreply.github.com>
2026-09-18 00:06:30 -0300

    chore(release): bump NanoMonitor agent to 1.4.0

--- agent/internal/version/version.go
+++ agent/internal/version/version.go
@@ -3,7 +3,7 @@ package version
 // These variables are set at build time via -ldflags
 var (
 	// Version is the semantic version of the agent
-	Version = "1.3.0"
+	Version = "1.4.0"
 	// Commit is the git commit hash
 	Commit = "unknown"
 	// BuildDate is the date the binary was built
